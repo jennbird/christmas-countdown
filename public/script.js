f2bf2abd27d9660a704e4abad1743a073fa7955f
Jenn Levy <jennbird91@aol.com>
2022-12-18 17:24:01 -0500

    give snowflakes random aesthetic qualities to make them look realistic

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@
 
 function daysUntilChristmas(){
 	const currentDate = new Date();
-	console.log(currentDate);
+	//console.log(currentDate);
 	const christmasTimeStamp = Date.parse("2022-12-25");
 	const christmas = new Date(christmasTimeStamp);
 	const msUntilChristmas = christmas - currentDate;
@@ -25,10 +25,10 @@ function daysUntilChristmas(){
 
 
 	//const hoursUntilChristmas = Math.floor(daysUntilChristmas / 24/ 60 / 60);
-	console.log(roundedDaysUntilChristmas + " days");
-	console.log(roundedHoursUntilChristmas + " hours");
-	console.log(roundedMinutesUntilChristmas + " minutes");
-	console.log(roundedSecondsUntilChristmas + " seconds left until CHRISTMAS!");
+	// console.log(roundedDaysUntilChristmas + " days");
+	// console.log(roundedHoursUntilChristmas + " hours");
+	// console.log(roundedMinutesUntilChristmas + " minutes");
+	// console.log(roundedSecondsUntilChristmas + " seconds left until CHRISTMAS!");
 
 	//const jorah = document.getElementById("days"); //hey html, get me the element with the id text, 
 	//jorah.innerText = daysUntilChristmas  // all the information about the element p is saved to the variable paragraph (*variable could be called anything doesn't have to be paragraph)	
@@ -78,23 +78,65 @@ if(currentDay <25){
 	console.log("It might be the 25...or it might be later");
 }*/
 
+
+let snowflakes = [];
+
+// generate snowflake data
+
+for (let i = 0; i < 100; i++) {
+	const snowflake = {
+		x: Math.random() * window.innerWidth,
+		y: 0,
+		speed: 3 + Math.random() * 2,
+		opacity: 0.2 + Math.random() * 0.8,
+		color: "white",
+		size: 5 + Math.random() * 5,
+	}
+	snowflakes.push(snowflake);
+};
+
+// create snowflake divs on screen
+
+
+
+
+// update the snowflake divs we created every 50ms
+
+for(let i = 0; i <snowflakes.length; i++){
+	const snowflakeDiv = document.createElement("div");
+	snowflakeDiv.className = "snowflake";
+	snowflakeDiv.id = i;
+	snowflakeDiv.style.top = snowflakes[i].y + "px";
+	snowflakeDiv.style.left = snowflakes[i].x + "px";
+	snowflakeDiv.style.opacity = snowflakes[i].opacity;
+	snowflakeDiv.style.height = snowflakes[i].size + "px";
+	snowflakeDiv.style.width = snowflakes[i].size + "px";
+
+	document.body.appendChild(snowflakeDiv);
+}
+
 // set up a loop
 
-let y = 0; 
-let x = Math.random() * window.innerWidth;
-const speed = 5;
+
 
 function gameLoop() {
 	const babyFlake = document.getElementById("babyFlake");
 	
-	y = y+speed;
-	if (y>window.innerHeight){
-		x = Math.random() * window.innerWidth;
-		y = 0;
+	for(let i = 0; i <snowflakes.length; i++){
+		snowflakes[i].y = snowflakes[i].y + snowflakes[i].speed;
+
+		const snowflakeDiv = document.getElementById(i);
+		snowflakeDiv.style.top = snowflakes[i].y + "px";
+	
+
+		if (snowflakes[i].y>window.innerHeight){
+			snowflakes[i].x = Math.random() * window.innerWidth;
+			snowflakes[i].y = 0;
+		}
 	}
 	
-	babyFlake.style.top = y + "px";  // 10 + "px" --> "10px" 
-	babyFlake.style.left = x + "px";
+	// babyFlake.style.top = y + "px";  // 10 + "px" --> "10px" 
+	// babyFlake.style.left = x + "px";
 }
 
 setInterval(gameLoop, 40);
